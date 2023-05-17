4f13a7f0548b095e816b3744937932700de8c8bf
Brandone <brandone.martins@gmail.com>
2023-05-17 11:46:29 +0200

    feat: Ajout de notifications in-memory pour faire des checks d'accessibilité

--- src/notifications/components/App.tsx
+++ src/notifications/components/App.tsx
@@ -1,9 +1,13 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import SearchableList from './SearchableList/SearchableList';
-import { APINotifications } from '../infrastructure/api-notifications';
+
+// import { APINotifications } from '../infrastructure/api-notifications';
+import { InMemoryNotifications } from '../infrastructure/in-memory-notifications';
 
 const queryClient = new QueryClient();
-const notifications = new APINotifications();
+
+//const notifications = new APINotifications();
+const notifications = new InMemoryNotifications();
 
 export function App() {
 	return <QueryClientProvider client={queryClient}>
